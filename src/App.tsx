6d3ff760593662d5d4baf921cce8bbdbfab04491
Harry Minsky <harry.minsky@gmail.com>
2024-04-04 02:35:13 -0400

    Add Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import './App.css'
+import "./App.css";
+import { Header } from "./components/Header";
 
-function App() {
+export const App = () => {
   return (
     <>
-     <div>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-     </div>
-    
+      <Header/>
     </>
-  )
+  );
 }
 
-export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,6 @@
+export const Header = () => (
+  <div className="mx-auto text-center">
+    <h1 className="text-6xl font-bold">NUMBER</h1>
+    <h2 className="text-2xl">Guess Today's Number!</h2>
+  </div>
+);
