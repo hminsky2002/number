cf3af64c131f40841c89c5542fb78634c1ee70dc
Harry Minsky <harry.minsky@gmail.com>
2024-04-05 23:17:29 -0400

    added subtitle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ export const Header = () => {
     <div className="mx-auto text-center">
       <h1 className="text-6xl font-bold">NUMBER</h1>
       <h2 className="text-2xl">You Get One Guess</h2>
+      <h2 className="text-2xl">The number is between 0 and 99</h2>
       <h2 className="text-xl">{`Day ${day}`}</h2>
     </div>
   );
